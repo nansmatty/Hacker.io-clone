46233eee911960b8b44f60e4203876095bf00221
nansmatty <narayanmaitysp1997@gmail.com>
2022-02-26 13:37:37 +0530

    no msg

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const cors = require('cors');
 const authRoute = require('./routes/auth');
 
 const app = express();
+
+dotenv.config();
 
 app.use(express.json());
 
